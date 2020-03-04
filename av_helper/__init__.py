8ed17fac03233529f6c1b4c2a3a04aee5f99696d
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-03-04 19:09:03 +0530

    Update version number

--- av_helper/__init__.py
+++ av_helper/__init__.py
@@ -3,5 +3,5 @@ from .merge_audio_video import merge_audio_video
 from .save_audio_video_file import save_audio_video_file
 
 __name__ = 'av_helper'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __author__ = 'Vibhu Agarwal'
